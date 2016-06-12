34034de45fe4f2b4e7535e046fb525e009c7d2cf
David <David-NF@users.noreply.github.com>
2016-06-12 19:09:10 +0200

    Create common.php

---
+++ nl/common.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* @package Topic description
+* @copyright (c) 2016 Rich McGirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'TOPIC_DESC'				=> 'Omschrijving',
+));
